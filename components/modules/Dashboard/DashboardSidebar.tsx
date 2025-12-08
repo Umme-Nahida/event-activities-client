4046b27ef583bd5f893b089aeda9e8f8252afb39
Nahida Akther <ummenahida20@gmail.com>
2025-12-09 03:11:41 +0600

    add sidebar  in dashboard page

---
+++ components/modules/Dashboard/DashboardSidebar.tsx
@@ -0,0 +1,23 @@
+import { getDefaultDashboardRoute } from "@/lib/auth-utils";
+import { getNavItemsByRole } from "@/lib/navItems.config";
+import { getUserInfo } from "@/services/auth/getUserInfo";
+import { NavSection } from "@/types/dashboard.interface";
+import DashboardSidebarContent from "./DashboardSidebarContent";
+import { IUserInfo } from "@/types/user.interface";
+
+const DashboardSidebar = async () => {
+  const userInfo = (await getUserInfo()) as IUserInfo;
+
+  const navItems: NavSection[] = getNavItemsByRole(userInfo.role);
+  const dashboardHome = getDefaultDashboardRoute(userInfo.role);
+
+  return (
+    <DashboardSidebarContent
+      userInfo={userInfo}
+      navItems={navItems}
+      dashboardHome={dashboardHome}
+    />
+  );
+};
+
+export default DashboardSidebar;
